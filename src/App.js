f8ad23fb56c88dca4a407c57b504d1b17d16d57d
tsukada-t <t.tsukada1122@gmail.com>
2021-09-23 10:42:20 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
-
-function App() {
+const App = () => {
+  const profiles = [
+    {
+      name: "Taro", age: 20
+    },
+    {
+      name: "Hanako", age: 5
+    },
+    {
+      name: "Jiro"
+    }
+  ]
   return (
+    <>
+    <p>Hello, World!</p>
     <div>
-      <p>Hello World!</p>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
+    </>
   );
+}
+
+const User = (props) => {
+  const {name = "ゲスト", age = "unknown"} = props;
+  return (
+    <p>I am {name}, and {age} years old!</p>
+  )
 }
 
 export default App;
